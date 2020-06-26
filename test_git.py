56a786ac0512cae130ac60b6eef3eec688b98665
abhinav-sorot <67314513+abhinav-sorot@users.noreply.github.com>
2020-06-26 13:42:52 +0530

    Update test_git.py

--- test_git.py
+++ test_git.py
@@ -3,16 +3,20 @@ from login_page import Login
 
 
 def test_create_git_repo():
-    login = Login()
-    home = HomePage()
-    username = input("enter username: ")
-    password = input("enter password: ")
-    repo_name = input("enter repo name to create: ")
-    issue_name = input("enter issue name to create: ")
-    comments = input("please provide comments: ")
-    login.perform_login(username, password)
-    repo_full_name = home.create_repository(repo_name)
-    issue_number = home.create_issue(issue_name)
-    home.comment_and_add_emoji(comments)
-    assert home.link_issue_via_comment(issue_number)
-    home.delete_repo(repo_full_name)
+    try:
+        login = Login()
+        home = HomePage()
+        username = input("enter git username: ")
+        password = input("enter git password: ")
+        repo_name = input("enter repo name to create: ")
+        issue_name = input("enter issue name to create: ")
+        comments = input("please provide comments: ")
+        login.perform_login(username, password)
+        repo_full_name = home.create_repository(repo_name)
+        issue_number = home.create_issue(issue_name)
+        home.comment_and_add_emoji(comments)
+        assert home.link_issue_via_comment(issue_number)
+        assert not home.delete_repo(repo_full_name)
+    except Exception as e:
+        print(e)
+        assert False
